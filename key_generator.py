4e428843839d3cc9d8fcc800163c05afaa144ede
Ellery Gopaoco <batfolder@sjc163n35.joh.private.cam.ac.uk>
2021-01-30 02:43:41 +0000

    updated prime generator

--- key_generator.py
+++ key_generator.py
@@ -1,14 +1,9 @@
-from random import choice, randint
+from random import randint
 import primes
 
-
-def find_primes(min, max):
-    prime_list = []
-    for n in range(0, max - 31, 30):
-        for i in [7 + n, 11 + n, 13 + n, 17 + n, 19 + n, 23 + n, 29 + n, 31 + n]:
-            if i in range(min, max - 1):
-                prime_list.append(i)
-    return prime_list
+"""Attempted implementation of the RSA Digital Signature Algorithm as documented by the U.S. Department of Commerce
+Section B.1.1: Key Pair Generation Using Extra Random Bits
+Create public and private keys for yourself"""
 
 
 def bits_to_integer(bits, N):
@@ -18,48 +13,43 @@ def bits_to_integer(bits, N):
     return integer
 
 
+def find_prime(length, input_seed):
+    finding = primes.ST_random_prime(length, input_seed)
+    good_prime = finding.prime_no
+    return good_prime
+
+
 class PairKey:
-    def __init__(self, L, N, seedlen):
+    def __init__(self, L, N, input_seed):
         """:param L: <int> Bit length of p
         :param N: <int> Bit length of q"""
-        self.L = L
-        self.N = N
 
-        self.seedlen = seedlen
-        first_seed = self.find_firstseed()
+        self.p = find_prime(L, input_seed)
+        self.q = find_prime(N, input_seed)
+        print(self.p)
+        print(self.q)
 
         validate = Validate()
-        validate.LN(self.L, self.N, self.seedlen)
+        self.L = validate.pq(self.p, L)
+        self.N = validate.pq(self.q, N)
+        print(self.L)
+        print(self.N)
 
-        p = int(self.find_p())
-        q = int(self.find_q(p))
-        g = int(self.find_g(p, q))
+        validate.LN(self.L, self.N)
+
+        g = int(self.find_g(self.p, self.q))
         c = self.find_c()
+        """ !-!-! validate.g() takes too damn long to run """
+        validate.g(self.p, self.q, g)
 
-        validate.g(p, q, g)
-
-        x = (c % (q - 1)) + 1
-        y = (x ** g) % p
-        validate.xy(x, y, p, q)
+        x = (c % (self.q - 1)) + 1
+        y = (x ** g) % self.p
+        validate.xy(x, y, self.p, self.q)
 
         print(x, y)
 
-    def find_firstseed(self):
-        firstseed = ''.join(randint(0, 1) for _ in range(2 ** (self.N - 1)))
-        return firstseed
-
-    def find_p(self):
-        p = find_primes(2 ** (self.L - 1), 2 ** self.L)
-        p_prime = choice(p)
-        return p_prime
-
-    def find_q(self, p):
-        q = [i for i in find_primes(p2 ** (self.N - 1), 2 ** self.N) if (p - 1) % i != 0]
-        q_prime = choice(q)
-        return q_prime
-
     def find_g(self, p, q):
-        e = (p - 1) / q
+        e = (p - 1) // q
         h = randint(1, (p - 1))
         g = (h ** e) % p
         if g == 1:
@@ -76,12 +66,21 @@ class Validate:
     def __init__(self):
         pass
 
-    def LN(self, L, N, seedlen):
+    def LN(self, L, N):
         min_L, min_N = 10, 5
         if L < min_L or N < min_N:
             raise Exception("(L, N) pair is INVALID")
-        if seedlen < N:
-            raise Exception("Seedlength is INVALID")
+
+    def pq(self, pq, ln):
+        """Adjusts bit length (L, N) so that the newly generated prime numbers fall within their default relative range"
+        :param pq: <int> Prime number p or q
+        :param ln: Bit length of p or q respectively"""
+        while not (2 ** (ln - 1) <= pq <= 2 ** ln):
+            if pq < 2 ** (ln - 1):
+                ln -= 1
+            elif pq > 2 ** ln:
+                ln += 1
+        return ln
 
     def g(self, p, q, g):
         if not 2 <= g <= (p - 1):
@@ -100,4 +99,4 @@ class Validate:
             print("SUCCESS")
 
 
-pair = PairKey(20, 15)
+pair = PairKey(20, 15, 927)
--- primes.py
+++ primes.py
@@ -2,8 +2,12 @@ from hashlib import sha1
 from math import gcd
 from sympy import isprime
 
+"""Generation of probable primes p and q using SHA1
+Implement as parameters for securely generating keys and signatures"""
+
 
 def hash_it(digit):
+    # Converts SHA1 hash to integer
     digit = str(digit)
     new_hash = sha1(digit.encode("utf-8")).hexdigest()
     return int(new_hash, 16)
@@ -14,19 +18,19 @@ class ST_random_prime:
     def __init__(self, length, input_seed):
         self.length = (length - 1) * 2
         self.prime_seed = input_seed
-        self.prime_gen_counter = 0
 
+        self.prime_gen_counter = 0
         self.outlen = 160
         self.iterations = (self.length // self.outlen) - 1
         self.old_counter = self.prime_gen_counter
 
-        self.find_c0()
+        self.prime_no = self.find_c0()
 
     def find_c0(self):
-        """Generate a pseudorandom integer c of length bits"""
+        # Generates a pseudorandom integer c of length bits
         c0 = hash_it(self.prime_seed) + hash_it(self.prime_seed + 1)
         prime_no = 0
-        """Set prime to the least odd integer greater than or equal to c"""
+        # Sets prime to the least odd integer greater than or equal to c
         while self.prime_gen_counter <= (4 * self.length) and prime_no == 0:
             c0 = 2 ** (self.length - 1) + c0 % 2 ** (self.length - 1)
             c0 = 2 * c0 // 2 + 1
@@ -34,23 +38,23 @@ class ST_random_prime:
             self.prime_seed = self.prime_seed + 2
             if isprime(c0):
                 prime_no = c0
-                print('SUCCESS 1', c0, self.prime_seed, self.prime_gen_counter)
-                self.find_c(c0)
+                return prime_no
         if self.prime_gen_counter > (4 * self.length):
             print('FAILURE', 0, 0, 0)
             pass
 
-    def find_c(self, c0):
-        """Generate a pseudorandom integer x in the interval [2 ** (self.length - 1) + 2 ** (self.length - 1)]"""
+    """ !-!-! The next half of the code takes too damn long to run """
+    def find_x(self, c0):
+        # Generates a pseudorandom integer x
         x = 0
         for i in range(self.iterations):
             x = x + (hash_it(self.prime_seed + i) * 2 ** (i * self.outlen))
         self.prime_seed = self.prime_seed + self.iterations + 1
         x = 2 ** (self.length - 1) + (x % 2 ** (self.length - 1))
-        self.t_repeat(c0, x)
+        self.find_c(c0, x)
 
-    def t_repeat(self, c0, x):
-        """"Generate a candidate prime c in the interval [2 ** (self.length - 1) + 2 ** (self.length - 1)]"""
+    def find_c(self, c0, x):
+        # Generates a candidate prime c
         z, c = 0, 1
         while 1 != gcd(z - 1, c) or 1 != (z ** c0) % c:
             t = x // (2 * c0)
@@ -59,7 +63,6 @@ class ST_random_prime:
             c = 2 * t * c0 + 1
             self.prime_gen_counter = self.prime_gen_counter + 1
 
-            """Test the candidate prime c for primality"""
             a = 0
             for j in range(self.iterations):
                 a = a + (hash_it(self.prime_seed + j) * 2 ** (j * self.outlen))
@@ -74,5 +77,3 @@ class ST_random_prime:
         if self.prime_gen_counter > (4 * self.length + self.old_counter):
             print('FAILURE', 0, 0, 0)
 
-
-prime = ST_random_prime(100, 9771)
