eb97760bacd461c50a857f10f5844d66466e33cd
Ellery Gopaoco <batfolder@sjc163n75.joh.private.cam.ac.uk>
2021-03-14 23:20:42 +0000

    Set up client-server communication with Asyncio

--- peer.py
+++ peer.py
@@ -1,211 +1,70 @@
-#!/usr/local/bin/python3.9
-
+import asyncio
 import socket
-import threading
-import traceback
-import json
+import ast
 
-ADDME = "JOIN"
-PING = "PING"
-GIVENAME = "NAME"
-PEERLIST = "LIST"
-TRANSACTION = "CASH"
-REQUESTCHAIN = "BLOB"
-REPLY = "REPL"
-ERROR = "ERRO"
+sisters = [8888, 8877, 8866, 8855]
+peerlist = []
 
 
-class Peer:
-    def __init__(self, maxpeers, serverport, myid=None, serverhost=None):
-        """Initialises a peer node
-        :param maxpeers: <int> Maximum number of peers that the node will maintain
-        :param myid: <str> Canonical identifier of the node
-        :param serverport: <int> Port on which the node listens for connections
-        :param serverhost: <str>"""
-        self.maxpeers = maxpeers
-        self.serverport = serverport
+def extract_data(data):
+    decoded = data.decode('utf-8')
+    msgtype, message = ast.literal_eval(decoded)
+    return msgtype, message
 
-        if serverhost:
-            self.serverhost = serverhost
-        else:
-            self.serverhost = socket.gethostbyname(socket.gethostname())
 
-        if myid:
-            self.myid = myid
-        else:
-            self.myid = f"{self.serverhost}:{self.serverport}"
+def process_message(msgtype, message):
+    packet = str((msgtype, message))
+    encoded = packet.encode('utf-8')
+    return encoded
 
-        self.shutdown = False
-        self.peerlock = threading.Lock
-        self.peers = {}
-        self.handlers = {"JOIN": self.handle_addme,
-                         "PING": self.handle_ping,
-                         "NAME": self.handle_givename,
-                         "LIST": self.handle_peerlist,
-                         "CASH": self.handle_transaction,
-                         "BLOB": self.handle_requestchain}
 
-    def mainloop(self):
-        """Continuous loop accepting connections. When an incoming connection is accepted, the method handle_peer is
-        called to handle communication with this connection on a new thread"""
-        s = self.makesocket(self.serverport)
-        s.settimeout(None)
-        print(f"Server started: {self.myid}")
+async def handle_echo(reader, writer):
+    data = await reader.read(1024)
+    msgtype, message = extract_data(data)
+    address = writer.get_extra_info('peername')
+    print(f"Received {msgtype}: {message!r} from {address!r}")
 
-        while not self.shutdown:
-            try:
-                # Client socket is distinct from the listening socket
-                print("Listening for connections...")
-                client_sock, client_add = s.accept()
-                client_sock.settimeout(None)
-                t = threading.Thread(target=self.handlepeer, args=[client_sock])
-                t.start()
-            except:
-                print("Exception in Peer.mainloop():")
-                traceback.print_exc()
+    writer.write(data)
+    await writer.drain()
+    writer.close()
+    print("Close the connection")
 
-        print("Main loop exiting...")
-        print("Side loop exiting...")
-        s.close()
 
-    def makesocket(self, port, backlog=5):
-        """Server-side operations: sets up a socket which listens for incoming connections from other peers"""
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(('', port))
-        s.listen(backlog)
-        return s
+async def send_echo(PEERHOST, PEERPORT, msgtype, message):
+    reader, writer = await asyncio.open_connection(PEERHOST, PEERPORT)
+    newpeer = (PEERHOST, PEERPORT)
+    if newpeer not in peerlist:
+        peerlist.append(newpeer)
+        print(f'{PEERHOST}:{PEERPORT} added to peer list')
 
-    def handlepeer(self, client_sock):
-        """Takes a newly formed connection, processes the request, and dispatches the request to an appropriate
-        handler for processing"""
-        print(f"New child {threading.currentThread().getName()}")
-        print(f"Connected {client_sock.getpeername()}")
-        host, port = client_sock.getpeername()
-        peercon = Connection(None, host, port, client_sock, debug=False)
+    print(f'Sending message {msgtype!r}: {message!r} to {newpeer!r}...')
+    packet = process_message(msgtype, message)
+    writer.write(packet)
+
+    data = await reader.read(1024)
+    replytype, reply = extract_data(data)
+    print(f'Received reply {replytype}: {reply!r} from {newpeer!r}')
+
+    await writer.drain()
+    writer.close()
+
+
+async def main(PORT, HOST=None):
+    if not HOST:
+        name = socket.gethostname()
+        HOST = socket.gethostbyname(name)
+
+    for port in sisters:
         try:
-            msgtype, msgdata = peercon.received()
-            if msgtype in self.handlers:
-                print(f"Handling peer message from {client_sock.getpeername()}: {msgtype}: {msgdata}")
-                self.handlers[msgtype](peercon, msgdata)
-            else:
-                print(f"{msgtype}: {msgdata}")
+            await send_echo('127.0.0.1', port, 'PING', 'PING')
         except:
-            print("Exception in Peer.handlepeer():")
-            traceback.print_exc()
+            pass
 
-    def buildpeers(self, peerhost, peerport, depth=3):
-        try:
-            _, peerid = self.sendpeer(peerhost, peerport, GIVENAME, '')
-            print(f"Connection to {peerid} established")
-            self.addpeer(peerhost, peerport, peerid)
+    server = await asyncio.start_server(handle_echo, HOST, PORT)
+    address = server.sockets[0].getsockname()
+    print(f'Serving on {address}')
 
-            while depth:
-                if len(self.peers) < self.maxpeers or self.maxpeers == 0:
-                    _, peerjson =  self.sendpeer(peerhost, peerport, PEERLIST, '')
-                    peerdict = json.loads(peerjson)
-                    for nextpeer, data in peerdict:
-                        if nextpeer not in self.peers and nextpeer != self.myid:
-                            nexthost, nextport = data
-                            self.addpeer(nextpeer, nexthost, nextport)
-                            self.buildpeers(nexthost, nextport, depth-1)
-                else:
-                    print("Maximum number of peers reached")
-        except:
-            print("Exception in Peer.buildpeers():")
-            traceback.print_exc()
+    async with server:
+        await server.serve_forever()
 
-    def sendpeer(self, host, port, msgtype, msgdata, peerid=None):
-        replies = []
-        try:
-            peercon = Connection(peerid, host, port)
-            sent = peercon.send(msgtype, msgdata)
-            if sent:
-                print(f"Request type {msgtype} sent. Waiting for reply...")
-                reply = peercon.received()
-                while reply != (None, None):
-                    print(f"Received reply from {peerid}: {reply}")
-                    reply = peercon.received()
-                    replies.append(reply)
-            peercon.close()
-        except:
-            print("Exception in Peer.sendpeer():")
-            traceback.print_exc()
-        return replies
-
-    def addpeer(self, peerid, host, port):
-        """Adds peer name and host:port mapping to the known list of peers"""
-        self.peers[peerid] = (host, port)
-        print(f"{peerid} has been added")
-        print(f"Requesting {peerid} to add me...")
-        i = (self.myid, self.serverhost, self.serverport)
-        self.sendpeer(host, port, ADDME, i)
-        return True
-
-    def handle_addme(self, peercon, data):
-        if self.maxpeers == 0 or self.maxpeers > len(self.peers):
-            peerid, host, port = data
-            answer = self.addpeer(peerid, host, port)
-            if answer:
-                peercon.send(REPLY, f"{peerid} has been added")
-        else:
-            print("Maximum number of peers reached")
-            peercon.send(ERROR, f"Maximum number of peers reached")
-
-    def handle_ping(self, peercon, data):
-        del data
-        peercon.send(REPLY, "PONG")
-
-    def handle_givename(self, peercon, data):
-        del data
-        peercon.send(REPLY, self.myid)
-
-    def handle_peerlist(self, peercon, data):
-        del data
-        peercon.send(REPLY, self.peers)
-
-    def handle_transaction(self, peercon, data):
-        pass
-
-    def handle_requestchain(self, peercon, data):
-        pass
-
-
-class Connection:
-    def __init__(self, peerid, host, port, sock=None, debug=False):
-        self.id = peerid
-        self.debug = debug
-        if not sock:
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.s.connect((host, port))
-        else:
-            self.s = sock
-
-    def send(self, msgtype, msgdata):
-        try:
-            msgjson = json.dumps((msgtype, msgdata))
-            self.s.send(bytes(msgjson, encoding='utf-8'))
-            return True
-        except:
-            print("Exception in Connection.send():")
-            traceback.print_exc()
-            return False
-
-    def received(self):
-        try:
-            fullmsg = ""
-            while True:
-                message = self.s.recv(1024)
-                if len(message) <= 0:
-                    break
-                fullmsg += message.decode('utf-8')
-            msgdata, msgtype = json.loads(fullmsg)
-            return msgdata, msgtype
-        except:
-            print("Exception in Connection.received():")
-            traceback.print_exc()
-            return None, None
-
-    def close(self):
-        """Closes the peer connection"""
-        self.s.close()
+asyncio.run(main(8888))
--- test1.py
+++ test1.py
@@ -1,22 +1,41 @@
-#!/usr/local/bin/python3.9
+import asyncio
+import ast
 
-import sys
-import peer
-import threading
-
-
-maxpeers = int(sys.argv[1])
-serverport = int(sys.argv[2])
-firstpeer = sys.argv[3]
+peerlist = [('127.0.0.1', 8877), ('127.0.0.1', 8866), ('127.0.0.1', 8855)]
 
 
-def peerstart(maxpeers, serverport, firstpeer):
-
-    node = peer.Peer(maxpeers, serverport)
-    host, port = firstpeer.split(":")
-
-    t = threading.Thread(target=node.mainloop, args=[])
-    t.start()
+def extract_data(data):
+    decoded = data.decode('utf-8')
+    msgtype, message = ast.literal_eval(decoded)
+    return msgtype, message
 
 
-peerstart(maxpeers, serverport, firstpeer)
+def process_message(msgtype, message):
+    packet = str((msgtype, message))
+    encoded = packet.encode('utf-8')
+    return encoded
+
+
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    msgtype, message = extract_data(data)
+    addr = writer.get_extra_info('peername')
+    print(f"Received {msgtype!r}:{message!r} from {addr!r}")
+
+    print(f'Sending reply {msgtype!r}: {message!r}...')
+    packet = process_message(msgtype, message)
+    writer.write(packet)
+    await writer.drain()
+    writer.close()
+
+
+async def main():
+    server = await asyncio.start_server(handle_echo, '127.0.0.1', 8888)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
+
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
